47bd438407c217ffad9affa394a4db0cb4974693
Boopathi Rajaa <me@boopathi.in>
2015-01-10 15:29:23 +0530

    Moves AJAX request to JSONP for working client-only APP

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var co = require('co'),
 	get = require('./src/GetRequest'),
 	mapInit = require('./src/GoogleMaps.js'),
 	updateTimeInterval = 1000,
+	api = "http://api.open-notify.org/iss-now.json",
 	map, issmarker;
 
 var updateLocation = function() {
-	get('/api/location')
-		.then(function(respStr){
-			var resp = JSON.parse(respStr),
-				center = new google.maps.LatLng(resp.iss_position.latitude, resp.iss_position.longitude);
+	return get(api, 'jsonp')
+		.then(function(resp){
+			var center = new google.maps.LatLng(resp.iss_position.latitude, resp.iss_position.longitude);
 			issmarker.setPosition(center);
 			map.panTo(center);
 			setTimeout(updateLocation, updateTimeInterval);
@@ -18,19 +18,20 @@ var updateLocation = function() {
 		});
 };
 
-co(function *() {
-	var respStr = yield get('/api/location');
-	return JSON.parse(respStr);
-}).then(function(resp) {
-	map = mapInit(resp.iss_position.latitude, resp.iss_position.longitude);
-	issmarker = new google.maps.Marker({
-		position: map.getCenter(),
-		icon: 'images/iss.png',
-		map: map
+document.addEventListener('DOMContentLoaded', function() {
+	co(function *() {
+		return yield get(api, 'jsonp');
+	}).then(function(resp) {
+		map = mapInit(resp.iss_position.latitude, resp.iss_position.longitude);
+		issmarker = new google.maps.Marker({
+			position: map.getCenter(),
+			icon: 'images/iss.png',
+			map: map
+		});
+		setTimeout(updateLocation, updateTimeInterval);
+	}).catch(function(err) {
+		console.log(err.stack);
 	});
-	setTimeout(updateLocation, updateTimeInterval);
-}).catch(function(err) {
-	console.log("Something went wrong :", err);
 });
 
 if('serviceWorker' in navigator) {
