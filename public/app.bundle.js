310ea635117347886925500d7c215edcdb7045d9
Boopathi Rajaa <me@boopathi.in>
2015-01-10 16:48:35 +0530

    Disables Default UI of google maps - disables controls

--- public/app.bundle.js
+++ public/app.bundle.js
@@ -129,7 +129,8 @@
 	module.exports = function(lat, lng) {
 		var mapOptions = {
 	        zoom: 5,
-	        center: new google.maps.LatLng(lat, lng)
+	        center: new google.maps.LatLng(lat, lng),
+	        disableDefaultUI: true
 	    };
 		return new google.maps.Map(document.getElementById('map-container'), mapOptions);
 	}
