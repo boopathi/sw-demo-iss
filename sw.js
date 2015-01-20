0061c602ad76ec16a2a4c367e3a706f28ccf083d
Boopathi Rajaa <me@boopathi.in>
2015-01-20 23:13:39 +0530

    Fixes service worker

--- sw.js
+++ sw.js
@@ -1,65 +1,23 @@
 importScripts("public/serviceworker-cache-polyfill.js");
 
-var CACHE_NAME = "isstracker-cache-v1";
+var CACHE_NAME = "sw-demo-iss-v1";
 
-var urlsToCache = [
-	// "index.html",
-	// "public/app.bundle.js",
-	// "public/sw.bundle.js",
-	// "public/serviceworker-cache-polyfill.js",
-	// "public/images/iss.png",
-	new Request("//api.wheretheiss.at/v1/satellites/25544", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/maps-api-v3/api/js/19/5/common.js", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/maps-api-v3/api/js/19/5/map.js", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/maps-api-v3/api/js/19/5/util.js", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/maps-api-v3/api/js/19/5/marker.js", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/maps-api-v3/api/js/19/5/infowindow.js", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/maps-api-v3/api/js/19/5/onion.js", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/mapfiles/openhand_8_8.cur", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/maps-api-v3/api/js/19/5/controls.js", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/mapfiles/transparent.png", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/maps-api-v3/api/js/19/5/stats.js", { mode: 'no-cors' }),
-	new Request("//maps.gstatic.com/mapfiles/api-3/images/google_white2.png", { mode: 'no-cors' })
-];
-
-self.addEventListener('install', function(event) {
-	console.log("installing...");
-	console.log(event);
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then(function(cache) {
-				console.log('cached');
-				return cache.addAll(urlsToCache);
-			})
-	);
+this.addEventListener('fetch', function(event) {
+	var fetchReq = event.request.clone(),
+		cacheReq = event.request.clone();
+	event.respondWith(fetch(fetchReq).then(function(response) {
+		var resp = response.clone(),
+			req = event.request.clone();
+		caches.open(CACHE_NAME).then(function(cache) {
+			cache.put(req, resp);
+		});
+		return response;
+	}).catch(function() {
+		return caches.match(cacheReq);
+	}));
 });
 
-self.addEventListener('activate', function(event) {
-	console.log('activation');
-	console.log(event);
-	var cacheWhiteList = [];
+this.addEventListener('activate', function(e) {
+	console.log('deleting cache');
+	e.waitUntil(caches.delete(CACHE_NAME));
 });
-
-// self.addEventListener('fetch', function(event) {
-// 	console.log('fetch happening...');
-// 	event.respondWith(
-// 		caches.match(event.request)
-// 			.then(function(response) {
-// 				if(response) return response;
-// 				var fetchRequest = event.request.clone();
-// 				return fetch(fetchRequest).then(function(response) {
-// 					if(!response || response.status!==200 || response.type !== 'basic') {
-// 						return response;
-// 					}
-// 					var responseToCache = response.clone();
-// 					caches.open(CACHE_NAME)
-// 						.then(function(cache) {
-// 							console.log('new response saved to cache.');
-// 							var cacheRequest = event.request.clone();
-// 							cache.put(cacheRequest, responseToCache);
-// 						});
-// 					return response;
-// 				});
-// 			})
-// 	);
-// });
